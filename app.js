deef84d3fe6878d9d9eba4aa8c9287dbd5e6797e
Denys Nazarenko <denysnazarenko23@gmail.com>
2022-02-06 14:25:14 +0200

    change endpoint api/operations

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use('/api/users', authRouter);
-app.use('/api/income', operationsRouter);
+app.use('/api/operations', operationsRouter); // Denys изменил эту строчку. Было так - app.use('/api/income', operationsRouter);
 
 
 app.use((req, res) => {
