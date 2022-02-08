eba3285b926bd9eef2c8fadf295ffb7379f70f8f
Denys Nazarenko <denysnazarenko23@gmail.com>
2022-02-08 19:32:47 +0200

    added delete-expense endpoint and validation id of incomes\expenses endpoints

--- routes/api/operations/validation.js
+++ routes/api/operations/validation.js
@@ -1,5 +1,8 @@
 import Joi from 'joi';
 import { HttpCode } from '../../../lib/constants.js';
+import pkg from 'mongoose';
+
+const { Types } = pkg;
 
 const addIncomeSchema = Joi.object({
   email: Joi.string().email().required(),
@@ -12,12 +15,6 @@ const addIncomeSchema = Joi.object({
 const updateBalanceSchema = Joi.object({
   balance: Joi.string().required(),
 });
-
-    time: Joi.string().max(100).required(),
-    count: Joi.string().max(100).required(),
-    category: Joi.string().max(1000).required(),
-    description: Joi.string().max(1000),
-})
 
 export const validateAddIncome = async (req, res, next) => {
   try {
@@ -37,6 +34,15 @@ export const validationUpdateBalance = async (req, res, next) => {
     return res
       .status(HttpCode.BAD_REQUEST)
       .json({ message: err.message.replace(/"/g, '') });
+  }
+  next();
+};
+
+export const validateId = async (req, res, next) => {
+  if (!Types.ObjectId.isValid(req.params.id)) {
+    return res
+      .status(HttpCode.BAD_REQUEST)
+      .json({ message: 'Invalid ObjectId' });
   }
   next();
 };
--- service/operations/index.js
+++ service/operations/index.js
@@ -1,16 +1,20 @@
 import repositoryOperations from '../../repository/operations';
-import repositoryUsers from '../../repository/users';
-
 class OperationsService {
-
-
   async addIncomeObject(id, body) {
-    const addIncomeObj = await repositoryOperations.createIncome({ owner: id, ...{ body } })
+    const addIncomeObj = await repositoryOperations.createIncome({
+      owner: id,
+      ...{ body },
+    });
   }
 
   async deleteIncome(userId, incomeId) {
     const income = await repositoryOperations.deleteIncome(userId, incomeId);
     return income;
+  }
+
+  async deleteExpense(userId, expenseId) {
+    const expense = await repositoryOperations.deleteExpense(userId, expenseId);
+    return expense;
   }
 }
 
