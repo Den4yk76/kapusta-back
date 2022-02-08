418091620cad23f9fdfb17aa5afb7dcf2964c905
Denys Nazarenko <denysnazarenko23@gmail.com>
2022-02-08 16:47:38 +0200

    changed fields names and types in incomes and expenses schemas

--- model/income.js
+++ model/income.js
@@ -1,49 +1,48 @@
 import pkg from 'mongoose';
 
-
 const { Schema, model } = pkg;
 
 const incomeSchema = new Schema(
-    {
-        owner: {
-            type: String,
-            required: true,
-            unique: false
-        },
-        time: {
-            type: String,
-            required: true,
-        },
-        count: {
-            type: String,
-            required: true,
-        },
-        category: {
-            type: String,
-            required: true,
-        },
-        description: {
-            type: String,
-            default: "",
-        },
+  {
+    owner: {
+      type: String,
+      required: true,
+      unique: false,
     },
-    {
-        versionKey: false,
-        toJSON: {
-            virtuals: true,
-            transform: function (doc, ret) {
-                delete ret._id;
-                return ret;
-            },
-        },
-        toObject: {
-            virtuals: true,
-            transform: function (doc, ret) {
-                delete ret._id;
-                return ret;
-            },
-        },
+    date: {
+      type: Number,
+      required: true,
     },
+    count: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+  },
+  {
+    versionKey: false,
+    toJSON: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        delete ret._id;
+        return ret;
+      },
+    },
+    toObject: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        delete ret._id;
+        return ret;
+      },
+    },
+  },
 );
 
 const Income = model('income', incomeSchema);
