0fc28155538b545674d52d2a8096e3bab7e25904
Denys Nazarenko <denysnazarenko23@gmail.com>
2022-02-11 21:18:45 +0200

    added google-auth functionality, added user balance to response from /login endpoint, added new http code and added it into google-auth endpoint response

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
-import { registration, login, logout } from '../../../controllers/auth';
+import {
+  googleLogin,
+  registration,
+  login,
+  logout,
+} from '../../../controllers/auth';
 import { validateAuth } from './validation';
 import guard from '../../../middlewares/guard';
 
 const router = new Router();
 
+router.post('/google-login', googleLogin);
 router.post('/signup', validateAuth, registration);
 router.post('/login', validateAuth, login);
 router.post('/logout', guard, logout);
--- service/auth/index.js
+++ service/auth/index.js
@@ -1,37 +1,41 @@
 import jwt from 'jsonwebtoken';
-import Users from '../../repository/users';
+import repositoryUsers from '../../repository/users';
 const SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 class AuthService {
   async isUserExist(email) {
-    const user = await Users.findByEmail(email);
+    const user = await repositoryUsers.findByEmail(email);
     return !!user;
-  };
+  }
 
   async create(body) {
-    const { email } = await Users.create(body);
+    const { email } = await repositoryUsers.create(body);
     return { email };
-  };
+  }
 
   async getUser(email, password) {
-    const user = await Users.findByEmail(email);
-    const isValidPassword = await user?.isValidPassword(password)
+    const user = await repositoryUsers.findByEmail(email);
+    const isValidPassword = await user?.isValidPassword(password);
     if (!isValidPassword) {
-      return null
+      return null;
     }
-    return user
-  };
+    return user;
+  }
 
   getToken(user) {
     const id = user.id;
     const payload = { id };
     const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '12h' });
-    return token
-  };
+    return token;
+  }
 
   async setToken(id, token) {
-    await Users.updateToken(id, token);
-  };
+    await repositoryUsers.updateToken(id, token);
+  }
+
+  async googleLogin(token) {
+    return await repositoryUsers.googleLogin(token);
+  }
 }
 
 export default AuthService;
