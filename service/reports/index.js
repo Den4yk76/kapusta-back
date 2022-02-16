3d1adfc8360a1dc99bba641d118e95ebe26aa22c
Denys Nazarenko <denysnazarenko23@gmail.com>
2022-02-16 16:44:01 +0200

    fixed Google authentication functionality and removed duplicate routes functionality

--- service/reports/index.js
+++ service/reports/index.js
@@ -1,24 +1,6 @@
 import repositoryReports from '../../repository/reports';
 
 class ReportsService {
-  async getMonthTransactions(unixStart, unixEnd, category) {
-    const result = await repositoryReports.findMonthTransactions(
-      unixStart,
-      unixEnd,
-      category,
-    );
-
-    if (result) {
-      for (const item of result) {
-        item['date'] = undefined;
-        item['owner'] = undefined;
-        item['id'] = undefined;
-      }
-
-      return result;
-    }
-  }
-
   async getMonthAmounts(unixStart, unixEnd) {
     const { incomes, expenses } = await repositoryReports.findMonthAmounts(
       unixStart,
